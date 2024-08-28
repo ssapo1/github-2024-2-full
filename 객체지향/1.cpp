a5952809154e9fe77cb4f0280e6278d393b986f3
ssapo1 <dltnwhd7426@naver.com>
2024-08-28 12:08:45 +0900

    new1

---
+++ 객체지향/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1, num2;
+    num1 =1 , num2 = 2;
+    std::cout<<"Hello world"<<endl;
+    cout << num1 << "+" << num2 << "="<< num1 + num2  ;
+    return 0 ;
+}
